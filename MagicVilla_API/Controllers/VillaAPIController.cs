6441fc95b1e4e9be5e70c37e85bc1ff35e3e038b
SoumikMahajan <soumik800@gmail.com>
2023-06-19 16:05:15 +0530

    Add project files.

---
+++ MagicVilla_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,196 @@
+﻿using AutoMapper;
+using MagicVilla_API.Logging;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
+using MagicVilla_API.Repository.IRepository;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        //private readonly ILogger<VillaAPIController> _logger;
+        //private readonly ApplicationDbContext _db;
+
+        //public VillaAPIController(ILogger<VillaAPIController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        private readonly ILogging _logger;
+        private readonly IVillaRepository _dbVilla;
+        private readonly IMapper _mapper;
+        public VillaAPIController(ILogging logger, IVillaRepository dbVilla, IMapper mapper)
+        {
+            _logger = logger;
+            _dbVilla = dbVilla;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<VillaDTO>>> GetVillas()
+        {
+            _logger.Log("Getting all villas", "");
+            //IEnumerable<Villa> villaList = await _db.Villas.ToListAsync();
+            IEnumerable<Villa> villaList = await _dbVilla.GetAllAsync();
+            return Ok(_mapper.Map<List<VillaDTO>>(villaList));
+        }
+
+        //If you do not define HTTP Verb,it defaults to "HTTPGET"
+        [HttpGet("id")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<VillaDTO>> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                _logger.Log("Get Villa Error with Id" + id, "error");
+                return BadRequest();
+            }
+            //var villa = await _db.Villas.FirstOrDefaultAsync(u => u.Id == id);
+
+            var villa = await _dbVilla.GetAsync(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<VillaDTO>(villa));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<VillaDTO>> CreateVilla([FromBody] VillaCreateDTO createDTO)
+        {
+            //if (ModelState.IsValid) {
+            //    return BadRequest(ModelState);
+            //}
+
+            if (await _dbVilla.GetAsync(u => u.Name.ToLower() == createDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa already Exists");
+                return BadRequest(ModelState);
+            }
+            if (createDTO == null)
+            {
+                return BadRequest(createDTO);
+            }
+            Villa model = _mapper.Map<Villa>(createDTO);
+
+            //Villa model = new()
+            //{
+            //    Amenity = createDTO.Amenity,
+            //    Details = createDTO.Details,
+            //    ImageUrl = createDTO.ImageUrl,
+            //    Name = createDTO.Name,
+            //    Occupancy = createDTO.Occupancy,
+            //    Rate = createDTO.Rate,
+            //    Sqft = createDTO.Sqft
+            //};
+
+            //await _db.Villas.AddAsync(model);
+            //await _db.SaveChangesAsync();
+
+            await _dbVilla.CreateAsync(model);
+
+            return CreatedAtRoute("GetVilla", new { id = model.Id }, model);
+
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = await _dbVilla.GetAsync(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            //_db.Villas.Remove(villa);
+            //await _db.SaveChangesAsync(true);
+
+            await _dbVilla.RemoveAsync(villa);
+
+            return NoContent();
+
+        }
+
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdateVilla(int id, [FromBody] VillaUpdateDTO updateDTO)
+        {
+            if (updateDTO == null || id != updateDTO.Id)
+            {
+                return BadRequest();
+            }
+
+            Villa model = _mapper.Map<Villa>(updateDTO);
+            //Villa model = new()
+            //{
+            //    Amenity = updateDTO.Amenity,
+            //    Details = updateDTO.Details,
+            //    Id = updateDTO.Id,
+            //    ImageUrl = updateDTO.ImageUrl,
+            //    Name = updateDTO.Name,
+            //    Occupancy = updateDTO.Occupancy,
+            //    Rate = updateDTO.Rate,
+            //    Sqft = updateDTO.Sqft
+            //};
+            //_db.Villas.Update(model);
+            //await _db.SaveChangesAsync();
+
+            await _dbVilla.UpdateAsync(model);
+            return NoContent();
+        }
+
+        [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdatePartialVilla(int id, JsonPatchDocument<VillaUpdateDTO> patchDto)
+        {
+            if (patchDto == null || id == 0)
+            {
+                return BadRequest();
+            }
+            //var villa = await _db.Villas.AsNoTracking().FirstOrDefaultAsync(u => u.Id == id);
+            var villa = await _dbVilla.GetAsync(u => u.Id == id, tracked: false);
+
+            VillaUpdateDTO villaDTO = _mapper.Map<VillaUpdateDTO>(villa);
+
+
+            if (villa == null)
+            {
+                return BadRequest();
+            }
+            patchDto.ApplyTo(villaDTO, ModelState);
+            Villa model = _mapper.Map<Villa>(villaDTO);
+
+            //_db.Villas.Update(model);
+            //await _db.SaveChangesAsync();
+
+            await _dbVilla.UpdateAsync(model);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return NoContent();
+        }
+
+    }
+}
---
+++ MagicVilla_API/Program.cs
@@ -0,0 +1,48 @@
+//using Serilog;
+
+using MagicVilla_API;
+using MagicVilla_API.Data;
+using MagicVilla_API.Logging;
+using MagicVilla_API.Repository;
+using MagicVilla_API.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Information().WriteTo.File("log/villaLogs.txt",rollingInterval:RollingInterval.Day).CreateLogger();
+
+//builder.Host.UseSerilog();
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
+builder.Services.AddScoped<IVillaRepository, VillaRepository>();
+builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+builder.Services.AddControllers(option =>
+{
+    //option.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ILogging, LoggingV2>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
